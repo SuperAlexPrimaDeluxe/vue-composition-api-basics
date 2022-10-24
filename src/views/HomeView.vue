aafb6689c7219c8e2ba0c4096bb41534949024a0
Alexandre Chevalier <alextechnocite@gmail.com>
2022-10-24 13:53:16 +0200

    Création d'un simple compteur en vue JS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <button @click="decreaseCounter" class="btn">-</button>
+    <span class="counter">{{ counter }}</span>
+    <button @click="increaseCounter" class="btn">+</button>
   </div>
 </template>
+
+<script>
+export default {
+data() {
+  return {
+    counter: 0
+  }
+},
+methods: {
+  increaseCounter() {
+    this.counter++;
+  },
+  decreaseCounter() {
+    this.counter--;
+  }
+},
+}
+</script>
+
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.button .counter {
+  font-size: 40px;
+  padding: 10px;
+}
+</style>
